41af7c061cd1264ed9ca93e8724e04361bb99feb
susu <28025920@qq.com>
2016-05-16 16:17:38 +0800

    实现了地理编码与反编码

---
+++ GeocodeAndReverse/GeocodeAndReverse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeocodeAndReverse
+//
+//  Created by  江苏 on 16/5/16.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GeocodeAndReverse/GeocodeAndReverse/ViewController.m
@@ -0,0 +1,77 @@
+//
+//  ViewController.m
+//  GeocodeAndReverse
+//
+//  Created by  江苏 on 16/5/16.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+
+
+@interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UITextView *detailTV;
+@property (strong, nonatomic) IBOutlet UITextField *latitudeTF;
+@property (strong, nonatomic) IBOutlet UITextField *longitudeTF;
+
+/*地理编码管理器*/
+@property(strong,nonatomic)CLGeocoder* geoCoder;
+
+@end
+
+@implementation ViewController
+
+#pragma mark--懒加载
+/*地理编码管理器*/
+-(CLGeocoder *)geoCoder
+{
+    if (_geoCoder==nil) {
+        _geoCoder=[[CLGeocoder alloc]init];
+    }
+    return _geoCoder;
+}
+
+/**
+ *  地理编码
+ */
+- (IBAction)geocoder:(UIButton *)sender {
+    
+    [self.geoCoder geocodeAddressString:self.detailTV.text completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        
+        CLPlacemark *mark=[placemarks firstObject];
+        
+//        self.detailTV.text=mark.name;
+        
+        self.latitudeTF.text=@(mark.location.coordinate.latitude).stringValue;
+        
+        self.longitudeTF.text=@(mark.location.coordinate.longitude).stringValue;
+        
+    }];
+}
+
+/**
+ *  反地理编码
+ */
+- (IBAction)geoReverseCode:(UIButton *)sender {
+    
+    CLLocation *location=[[CLLocation alloc]initWithLatitude:self.latitudeTF.text.doubleValue longitude:self.longitudeTF.text.doubleValue];
+    
+    [self.geoCoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        
+        CLPlacemark *mark=[placemarks firstObject];
+        
+        self.detailTV.text=mark.name;
+//        
+//        self.latitudeTF.text=@(mark.location.coordinate.latitude).stringValue;
+//        
+//        self.longitudeTF.text=@(mark.location.coordinate.longitude).stringValue;
+        
+    }];
+    
+}
+
+
+
+@end
